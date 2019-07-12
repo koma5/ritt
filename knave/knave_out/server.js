2fb488558d70d7c0245d9fbd5abc559f054b44d1
Marco Koch <marco@5th.ch>
2019-07-12 19:29:16 +0100

    spit out default track on / .

--- knave/knave_out/server.js
+++ knave/knave_out/server.js
@@ -1,28 +1,42 @@
 const Influx = require('influxdb-nodejs');
+const express = require('express');
+
 const client = new Influx('http://influx_knave:8086/telegraf');
 
-client.query('mqtt_consumer')
-    .where('topic','ch/5th/ritt/default')
-    .then(function (data) {
+var app = express();
 
-        var geo = {
-            'type': 'FeatureCollection',
-            'features': [{
-                'type': 'Feature',
-                'properties': {},
-                'geometry': {
-                    'type': 'LineString',
-                    'coordinates': []
-                }
-            }]
-        }
+app.get('/', function(req, res) {
+    getGeojson('ch/5th/ritt/default', function(geojson) {
+        res.header("Content-Type", "application/geo+json");
+        res.send(geojson);
+    });
+});
 
-        data.results[0].series[0].values.forEach(function(row){
-            point = [row[3].split(',')];
-            geo.features[0].geometry.coordinates.push(point)
+app.listen(4432);
+console.log('Express started on port 4432');
+
+function getGeojson(topic, callback) {
+    client.query('mqtt_consumer')
+        .where('topic', topic)
+        .then(function (data) {
+
+            var geo = {
+                'type': 'FeatureCollection',
+                'features': [{
+                    'type': 'Feature',
+                    'properties': {},
+                    'geometry': {
+                        'type': 'LineString',
+                        'coordinates': []
+                    }
+                }]
+            }
+
+            data.results[0].series[0].values.forEach(function(row){
+                point = row[3].split(',');
+                geo.features[0].geometry.coordinates.push(point)
+            })
+            callback(geo);
         })
-        console.log(geo);
-    })
-    .catch(console.error);
-
-
+        .catch(console.error);
+}
