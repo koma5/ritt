3621b6991778dad24051361b2be71ad44bea0e0b
Marco Koch <marco@5th.ch>
2019-07-12 18:59:44 +0100

    geojson.

--- knave/knave_out/server.js
+++ knave/knave_out/server.js
@@ -2,13 +2,27 @@ const Influx = require('influxdb-nodejs');
 const client = new Influx('http://influx_knave:8086/telegraf');
 
 client.query('mqtt_consumer')
-  .where('topic','ch/5th/ritt/default')
-  .then(function (r) {
+    .where('topic','ch/5th/ritt/default')
+    .then(function (data) {
 
-    r.results[0].series[0].values.forEach(function(row){
-        console.log(row[3]);
+        var geo = {
+            'type': 'FeatureCollection',
+            'features': [{
+                'type': 'Feature',
+                'properties': {},
+                'geometry': {
+                    'type': 'LineString',
+                    'coordinates': []
+                }
+            }]
+        }
+
+        data.results[0].series[0].values.forEach(function(row){
+            point = [row[3].split(',')];
+            geo.features[0].geometry.coordinates.push(point)
+        })
+        console.log(geo);
     })
-  })
-  .catch(console.error);
+    .catch(console.error);
 
 
